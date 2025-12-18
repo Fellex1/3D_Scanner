ecb69c9c69726db7001a7745b31a6da2559251c1
Fellex1 <graneggerfelix@gmail.com>
2025-12-18 20:10:09 +0100

    Remove unused barcode image, add scale calibration placeholder, and enhance interface with new translations and restart functionality. Update message boxes for better user feedback.

--- Python_Files/Prototyp_GUI/Gewichts_Messung.py
+++ Python_Files/Prototyp_GUI/Gewichts_Messung.py
@@ -3,5 +3,9 @@
 def get_weight():
     return 5  # Beispielwert in kg
 
+def calibrate_scale():
+    pass  # Platzhalter für Kalibrierungslogik
+
+
 if __name__ == "__main__":
     print(get_weight())  # optional, für direkten Test
